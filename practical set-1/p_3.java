1afdcb61dc216de3ff213069dbf0562e9c36a293
krishbhalani007-commits <krishbhalani007@gmail.com>
2026-04-10 18:03:04 +0530

    Create p_3.java

---
+++ practical set-1/p_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pr_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a letter: ");
+        char ch = sc.next().charAt(0);
+
+        ch = Character.toLowerCase(ch);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+}
